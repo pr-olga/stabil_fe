a9f26122697a271c63915861cb62187099b9b2cf
pr-olga <frkhorkovaolga@gmail.com>
2020-07-18 15:40:44 +0200

    feat: add image to user profile

--- src/views/Pages/Users/UserProfile.vue
+++ src/views/Pages/Users/UserProfile.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="user-profile">
     <h1>Profile of User</h1>
- {{ this.user.players }}
-  <ul class="list-group">
-  <li class="list-group-item">Name: {{this.user.name}}</li>
-  <li class="list-group-item" v-if="this.user.players">Number of games: {{this.user.players.length}}</li>
-  <li class="list-group-item">Victories: </li>
-  <li class="list-group-item">Lost: </li>
-  <li class="list-group-item">Member since: </li>
-</ul>
-
+    <div class="container">
+      <div class="row">
+        <div class="col-md-2"><img src="../../../assets/profile_placeholder_cropped.jpg" class="rounded-circle user-profile__img" alt="Circle image"></div>
+        <div class="col-md-10">
+          <ul class="list-group">
+            <li class="list-group-item">Profile ID: {{this.user.id}}</li>
+            <li class="list-group-item">Name: {{this.user.name}}</li>
+            <li class="list-group-item" v-if="this.user.players">Number of games: {{this.user.players.length}}</li>
+            <li class="list-group-item">Victories: </li>
+            <li class="list-group-item">Lost: </li>
+            <li class="list-group-item">Member since: {{this.user.created}}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
-
 </template>
 
 <script>
@@ -28,7 +33,7 @@ export default {
       try {
         await UserService.getUser(this.$route.params.id).then((values) => {
           this.user = values.data
-          // console.log(this.user)
+          console.log(this.user)
         })
       } catch (error) {
         console.log(error)
@@ -41,6 +46,9 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.user-profile__img {
+  height: 150px;
+  width: 150px;
+}
 </style>
