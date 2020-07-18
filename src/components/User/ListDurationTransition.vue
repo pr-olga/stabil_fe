d0b6be7800c0b5f7c44cbdc4c681f097a5ea33d7
pr-olga <frkhorkovaolga@gmail.com>
2020-07-18 18:05:13 +0200

    feat: add animation to user profile

---
+++ src/components/User/ListDurationTransition.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="list-duration-transition">
+                <transition name="scale"
+              enter-active-class="scaleIn"
+              leave-active-class="scaleOut"
+              appear
+              @before-appear="setDuration"
+              @after-appear="cleanUpDuration"
+              >
+                  <slot></slot>
+              </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    duration: {
+      type: Number,
+      default: 300
+    }
+  },
+  methods: {
+    setDuration (el) {
+      el.style.animationDuration = `${this.duration}ms`
+    },
+    cleanUpDuration (el) {
+      el.style.animationDuration = ''
+    }
+  }
+}
+</script>
+
+<style>
+@keyframes scaleIn {
+  from {
+    transform: scale(0);
+  }
+  to {
+    transform: scale(1);
+  }
+}
+
+@keyframes scaleOut {
+  from {
+     transform: scale(1);
+  }
+  to {
+     transform: scale(0);
+  }
+}
+
+.scaleIn {
+  animation-name: scaleIn;
+}
+
+.scaleOut {
+  animation-name: scaleOut;
+}
+</style>
--- src/views/Pages/Users/UserProfile.vue
+++ src/views/Pages/Users/UserProfile.vue
@@ -1,18 +1,30 @@
 <template>
   <div id="user-profile">
-    <h1>Profile of User</h1>
+    <h1>User Profile</h1>
     <div class="container">
       <div class="row">
         <div class="col-md-2"><img src="../../../assets/profile_placeholder_cropped.jpg" class="rounded-circle user-profile__img" alt="Circle image"></div>
         <div class="col-md-10">
-          <ul class="list-group">
-            <li class="list-group-item">Profile ID: {{this.user.id}}</li>
-            <li class="list-group-item">Name: {{this.user.name}}</li>
-            <li class="list-group-item" v-if="this.user.players">Number of games: {{this.user.players.length}}</li>
-            <li class="list-group-item">Victories: </li>
-            <li class="list-group-item">Lost: </li>
-            <li class="list-group-item">Member since: {{this.user.created}}</li>
-          </ul>
+              <ul>
+                <list-transition :duration="duration[0]">
+                  <li class="list-group-item">Profile ID: {{this.user.id}}</li>
+                 </list-transition>
+                <list-transition :duration="duration[1]">
+                  <li class="list-group-item">Name: {{this.user.name}}</li>
+                 </list-transition>
+                <list-transition :duration="duration[2]">
+                  <li class="list-group-item" v-if="this.user.players">Number of games: {{this.user.players.length}}</li>
+                </list-transition>
+                   <list-transition :duration="duration[3]">
+                  <li class="list-group-item">Victories: </li>
+                 </list-transition>
+                 <list-transition :duration="duration[4]">
+                  <li class="list-group-item">Lost: </li>
+                </list-transition>
+                <list-transition :duration="duration[5]">
+                  <li class="list-group-item">Member since: {{this.user.created}}</li>
+                </list-transition>
+              </ul>
         </div>
       </div>
     </div>
@@ -21,19 +33,23 @@
 
 <script>
 import UserService from '@/services/UserService'
+import ListDurationTransition from '@/components/User/ListDurationTransition'
 
 export default {
   data () {
     return {
-      user: []
+      user: [],
+      duration: [300, 500, 700, 900, 1000, 1100] // make the stuff dynamically
     }
+  },
+  components: {
+    listTransition: ListDurationTransition
   },
   methods: {
     async getUser () {
       try {
         await UserService.getUser(this.$route.params.id).then((values) => {
           this.user = values.data
-          console.log(this.user)
         })
       } catch (error) {
         console.log(error)
@@ -42,7 +58,7 @@ export default {
   },
   created () {
     return this.getUser()
-  }
+  } // Object.keys(this.errors).length
 }
 </script>
 
