08c43e46c069532458c2576cc99d3cf7441943ca
pr-olga <frkhorkovaolga@gmail.com>
2020-08-09 12:30:29 +0200

    feat: set prelminary svg logo

--- src/views/Layout/Header.vue
+++ src/views/Layout/Header.vue
@@ -1,31 +1,28 @@
 <template>
-  <div id="header" >
-      <div class="logo">
-        <a href="/" class="active logo-text">stabiL <span class="kugel"></span></a>
-      </div>
-      <ul class="nav navbar">
-        <router-link to='/matches' class="nav-item" tag="li" active-class="active"><a class="nav-link">Matches</a>
-        </router-link>
-        <router-link to='/games' class="nav-item" tag="li" active-class="active"><a class="nav-link">Games</a>
-        </router-link>
-        <router-link to='/users' class="nav-item" tag="li" active-class="active"><a class="nav-link">Users</a>
-        </router-link>
-        <router-link to='/locations' class="nav-item" tag="li" active-class="active"><a class="nav-link">Locations</a>
-        </router-link>
-        <li class="nav-item">
-          <button class="btn-navi rotate-on-click"><img
-              src="https://ssl.gstatic.com/bt/C3341AA7A1A076756462EE2E5CD71C11/2x/btw_ic_speeddial_white_24dp_2x.png"
-              alt=""/></button>
-        </li>
-      </ul>
-    </div>
+<div id="header">
+  <div class="logo">
+    <a href="/"><img src="../../assets/logo.svg" alt="" width="150px"></a>
+  </div>
+  <ul class="nav navbar">
+    <router-link to='/matches' class="nav-item" tag="li" active-class="active"><a class="nav-link">Matches</a>
+    </router-link>
+    <router-link to='/games' class="nav-item" tag="li" active-class="active"><a class="nav-link">Games</a>
+    </router-link>
+    <router-link to='/users' class="nav-item" tag="li" active-class="active"><a class="nav-link">Users</a>
+    </router-link>
+    <router-link to='/locations' class="nav-item" tag="li" active-class="active"><a class="nav-link">Locations</a>
+    </router-link>
+    <li class="nav-item">
+      <button class="btn-navi rotate-on-click"><img src="https://ssl.gstatic.com/bt/C3341AA7A1A076756462EE2E5CD71C11/2x/btw_ic_speeddial_white_24dp_2x.png" alt="" /></button>
+    </li>
+  </ul>
+</div>
 </template>
 
 <script>
 export default {
 
 }
-
 </script>
 
 <style lang="scss" scoped>
@@ -37,32 +34,7 @@ export default {
 }
 
 .logo {
-  padding-top: 2rem;
-
-}
-
-.logo-text {
-   font-family: 'Mulish';
-
-  font-size: 36px;
-  color: #212529;
-  font-weight: 400;
-
-  &:hover {
-    color: #ff284a;
-    text-decoration: none;
-    transition: 0.3s;
-  }
-}
-
-.kugel {
-  display: inline-block;
-  width: 20px;
-  height: 20px;
-  margin-bottom: -6px;
-  margin-left: -8px;
-  background-color: #ff284a;
-  border-radius: 50%;
+  padding-top: 2.5rem;
 
 }
 
@@ -100,11 +72,11 @@ export default {
   width: 50px;
   background-color: #ff284a;
   text-align: center;
-  }
+}
 
 .rotate-on-click:focus {
   animation-name: rotateAnimation;
-  animation-duration:1s;
+  animation-duration: 1s;
   animation-iteration-count: 1;
   animation-timing-function: ease-in-out;
   animation-fill-mode: forwards;
@@ -112,8 +84,9 @@ export default {
 
 @keyframes rotateAnimation {
   from {
-     transition: rotate(0deg);
+    transition: rotate(0deg);
   }
+
   to {
     transform: rotate(360deg);
   }
@@ -128,5 +101,4 @@ button>img {
   -transform: rotate(180deg);
   transform: rotate(180deg);
 }
-
 </style>
