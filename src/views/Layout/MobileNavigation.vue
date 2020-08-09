b7d141fa0b36c2aa3f97fe5027a19bb3d16ba95b
pr-olga <frkhorkovaolga@gmail.com>
2020-08-09 21:16:42 +0200

    feat: add settomeout close animation

--- src/views/Layout/MobileNavigation.vue
+++ src/views/Layout/MobileNavigation.vue
@@ -2,20 +2,20 @@
   <div id="navigation-mobile">
     <div class="nav-icon" @click="closeMenu()"><img src="../../assets/times-solid.svg" alt="" height="30px"></div>
   <ul class="nav navbar">
-    <list-transition :duration="duration[0]">
-      <router-link to='/matches' class="nav-item" tag="li" active-class="active"><a class="nav-link">Matches</a>
+    <list-transition :duration="duration[0]"  >
+      <router-link to='/matches' class="nav-item" tag="li" active-class="active"><a class="nav-link" @click="closeMenu()">Matches</a>
       </router-link>
     </list-transition>
     <list-transition :duration="duration[1]">
-      <router-link to='/games' class="nav-item" tag="li" active-class="active"><a class="nav-link">Games</a>
+      <router-link to='/games' class="nav-item" tag="li" active-class="active"><a class="nav-link" @click="closeMenu()">Games</a>
       </router-link>
      </list-transition>
     <list-transition :duration="duration[2]">
-      <router-link to='/users' class="nav-item" tag="li" active-class="active"><a class="nav-link">Users</a>
+      <router-link to='/users' class="nav-item" tag="li" active-class="active"><a class="nav-link"  @click="closeMenu()">Users</a>
       </router-link>
      </list-transition>
      <list-transition :duration="duration[3]">
-      <router-link to='/locations' class="nav-item" tag="li" active-class="active"><a class="nav-link">Locations</a>
+      <router-link to='/locations' class="nav-item" tag="li" active-class="active"><a class="nav-link"  @click="closeMenu()">Locations</a>
       </router-link>
      </list-transition>
   </ul>
@@ -36,8 +36,10 @@ export default {
     listTransition: ListDurationTransition
   },
   methods: {
-    closeMenu () {
-      this.$emit('closeMenu', this.opened)
+    closeMenu (e) {
+      setTimeout((e) => {
+        this.$emit('closeMenu', this.opened) // TODO: refactor, add cool effect
+      }, 300)
     }
   }
 }
@@ -53,7 +55,7 @@ export default {
   z-index: 2;
   background-color: #f5f5fa;
   overflow: hidden;
-  transition: 0.5s;
+  transition: 1s;
 
   .navbar {
     flex-direction: column;
